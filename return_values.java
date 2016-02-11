b524f1e1909123ebd037c7114ed8523b9a079c50
RajviM <rajvimehta238@gmail.com>
2016-02-10 22:40:10 -0800

    Create return_values.java

---
+++ return_values.java
@@ -0,0 +1,21 @@
+class return_values
+{
+	private int return_eval_value;
+	private int alpha_beta;
+	public int getAlpha_beta() {
+		return alpha_beta;
+	}
+	
+	public return_values(int return_eval_value, int alpha_beta) {
+		super();
+		this.return_eval_value = return_eval_value;
+		this.alpha_beta = alpha_beta;
+	}
+
+	public int getReturn_eval_value() {
+		return return_eval_value;
+	}
+	
+		
+	
+}
